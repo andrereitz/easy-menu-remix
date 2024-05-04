b4ae09e29f55d866a9baf7c703280a8d84156bc4
Andre Reitz <andrereitz@live.com>
2024-05-04 13:12:46 -0300

    Adding shadcn ui

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,12 @@
 import type { MetaFunction } from "@remix-run/node";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
 
 export const meta: MetaFunction = () => {
   return [
@@ -10,7 +18,16 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
+      <h1>Welcome to Remix</h1><Badge variant="secondary">Secondary</Badge><Button>test</Button>
+      <Accordion type="single" collapsible>
+      <AccordionItem value="item-1">
+          <AccordionTrigger>Is it accessible?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It adheres to the WAI-ARIA design pattern.
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+
       <ul>
         <li>
           <a
